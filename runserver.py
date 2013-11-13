aa69310bc1f0b62c936793a8a6826c8c138ebd9f
Luis Flores <luis@luisflores.mx>
2013-11-13 14:45:18 +0000

    Organize imports

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
+import os
 from luisflores import app
-import os from pml 
 
-port = int(os.environ.get('PORT', 5000)) 
-app.run(host='0.0.0.0', port=port)
+os_port = int(os.environ.get('PORT', 5000)) 
+app.run(host='0.0.0.0', port=os_port)
