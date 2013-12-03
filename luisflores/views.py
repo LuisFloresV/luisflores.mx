a87b703d90994dc2135da46704ce89035087048b
Luis Flores <me@luisflores.mx>
2013-12-03 23:43:18 +0000

    Add base template and add content

--- luisflores/views.py
+++ luisflores/views.py
@@ -1,10 +1,12 @@
 from luisflores import app
 from flask import render_template
 
-@app.route('/')
-def index():
-    return render_template('base.html')
+# TODO:
+# @app.route('/')
+# def index():
+#     return render_template('index.html',home='active')
 
-@app.route('/about')
+# @app.route('/about')
+@app.route('/')
 def about():
-	return render_template('/about.html')
+	return render_template('about.html',about='active')
