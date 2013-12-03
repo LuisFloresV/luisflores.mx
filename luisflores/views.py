5c0932b045d8d4c17477825822c2488d7e0022c2
Luis Flores <me@luisflores.mx>
2013-12-03 23:45:01 +0000

    Add index page

--- luisflores/views.py
+++ luisflores/views.py
@@ -1,12 +1,10 @@
 from luisflores import app
 from flask import render_template
 
-# TODO:
-# @app.route('/')
-# def index():
-#     return render_template('index.html',home='active')
-
-# @app.route('/about')
 @app.route('/')
+def index():
+    return render_template('index.html',home='active')
+
+@app.route('/about')
 def about():
 	return render_template('about.html',about='active')
