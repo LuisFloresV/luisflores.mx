df8a57a8c1dd0d91f13a55bf22a1ae31c9310339
Luis Flores <me@luisflores.mx>
2013-12-04 19:27:50 +0000

    Add google analytics

--- luisflores/views.py
+++ luisflores/views.py
@@ -2,9 +2,5 @@ from luisflores import app
 from flask import render_template
 
 @app.route('/')
-def index():
-    return render_template('index.html',home='active')
-
-@app.route('/about')
 def about():
 	return render_template('about.html',about='active')
