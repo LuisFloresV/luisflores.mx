09440b10d797b12f8ba386ee93756d1221c7bb23
Luis Flores <me@luisflores.mx>
2013-12-20 00:35:06 +0000

    Add restart server script

---
+++ restart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+date +"%c" >> restart.log
+
+ID=$(pidof python)
+kill -9 $ID
+
+STATUS=$?
+
+if [ $STATUS != 0 ]
+then
+	echo "Could not kill process with PID: $ID" >> restart.log
+	return
+fi
+
+nohup /root/luisflores.mx/venv/bin/python runserver.py > /dev/null 2>&1 &
+
+STATUS=$?
+
+if [ $STATUS != 0 ]
+then
+	echo "Could not start server" >> restart.log
+fi
+
+echo "Server started" >> restart.log
