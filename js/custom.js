533704adad86d95a4373ac1b1864058b958ce299
Luis Flores <me@luisflores.mx>
2014-05-31 23:57:00 +0000

    Prepare migration

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,58 @@
-//jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 50) {
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//     } else {
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//     }
-// });
-
 $(document).ready(function() {
   $('.gallery').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
-    gallery:{
+    gallery: {
       enabled:true
     }
-    // other options
   });
 });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(document).ready(function() {
+    $("#ajaxSelect1").heapbox();
+    $("#ajaxSelect2").heapbox();
+    $("#ajaxSelect3").heapbox();
+
+
+  // json = '[{"value":"dynamic_value_1","text":"Dynamic value 1"},{"value":"dynamic_value_2","text":"Dynamic value 2"}]';
+  // $("#ajaxSelect").heapbox("set",json);
+
+
+  // // init heapbox for #ajaxSelect and define onChange callback with ajax
+  // $("#ajaxSelect").heapbox({
+  //     'onChange':function(value) {
+  //       //hide heapbox
+  //       $("#ajaxSelect2").heapbox("hide");
+  //
+  //      if(value) {
+  //        showAjaxSelect2(json);
+  //        $("#ajaxSelect2").heapbox("show");
+  //
+  //       }
+  //     }
+  // });
+  //
+  // //init heapbox for #ajaxSelect2
+  // $("#ajaxSelect2").heapbox({
+  //  'onChange': function(value) {
+  //    showMessage();
+  //   }
+  // });
+  //
+  // //hide heapbox
+  // $("#ajaxSelect2").heapbox("hide");
+  //
+  // //set data to heapbox for #ajaxSelect2
+  // function showAjaxSelect2(data) {
+  //    $("#ajaxSelect2").heapbox("set",data);
+  // }
+  //
+  // //show selected values
+  // function showMessage() {
+  //   foodType = $("#ajaxSelect option:selected").attr("value");
+  //   food = $("#ajaxSelect2 option:selected").attr("value");
+  //
+  //   alert("Cool, you are into "+foodType+", especially "+food+".");
+  // }
+
 });
